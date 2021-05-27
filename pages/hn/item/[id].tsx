a6807e670e16b6715760cf1fbe9c61167af46aab
David Chin <dlcmhd@me.com>
2021-05-27 18:14:58 +0800

    feature

--- pages/hn/item/[id].tsx
+++ pages/hn/item/[id].tsx
@@ -1,4 +1,23 @@
 import { HnItem } from 'containers/Hn/HnItem';
+import { GetServerSideProps } from 'next';
+
+// export async function getServerSideProps(context: NextPageContext) {
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  // // Fetch data from external API
+  // const res = await fetch(`https://.../data`)
+  // const data = await res.json()
+
+  // // Pass data to the page via props
+  // return { props: { data } }
+
+  const {
+    params: { id },
+  } = context;
+
+  console.log({ id });
+
+  return { props: {} };
+};
 
 export default function hnItem() {
   return <HnItem />;

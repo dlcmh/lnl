ace9f8ea71a25bd29e4d71e52092a6e062a685f7
David Chin <dlcmhd@me.com>
2021-05-29 10:03:49 +0800

    feature

--- pages/socket-io.tsx
+++ pages/socket-io.tsx
@@ -1,3 +1,18 @@
+import { PageWrapper } from 'components/PageWrapper/PageWrapper';
+import { useEffect } from 'react';
+import io from 'socket.io-client';
+
 export default function socketIo() {
-  return <p>Socket.io Demo</p>;
+  useEffect(() => {
+    fetch('/api/socketio').finally(() => {
+      const socket = io();
+      console.log('dlc socket', socket);
+    });
+  }, []);
+
+  return (
+    <PageWrapper title="Socket.io Demo">
+      <p>Socket.io Demo</p>
+    </PageWrapper>
+  );
 }
